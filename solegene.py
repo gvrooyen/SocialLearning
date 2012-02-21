57a9722370ffaf961310c3d3c86f33c803968752
G-J van Rooyen <gvrooyen@sun.ac.za>
2012-02-21 08:41:12 +0200

    Corrected bug in the previous commit

--- solegene.py
+++ solegene.py
@@ -34,6 +34,7 @@ try:
 except ImportError:
     class NullHandler(logging.Handler):
         def emit(self, record):
+            pass
 
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
