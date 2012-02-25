0ca3c11543d98608fa38cace77c6c8d8ce83e980
G-J van Rooyen <gvrooyen@sun.ac.za>
2012-02-26 00:28:19 +0200

    Updated post_jobs.py

--- master.py
+++
@@ -1,7 +0,0 @@
-import rungp
-import cloud
-
-# nice python rungp.py --debug -d ORd1 --mode_model_bias --mode_cumulative
-jid = cloud.call(rungp.rungp, debug = True, d = 'ORd1', mode_model_bias = True, mode_cumulative = True)
-
-cloud.join([jid])
