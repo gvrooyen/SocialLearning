01178ef9d50c9b0de42c3a990f89c031be0494bf
G-J van Rooyen <gvrooyen@sun.ac.za>
2012-02-26 12:50:55 +0200

    Wrote a saner persistent daemon script.

--- daemon.sh
+++ daemon.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-nohup python daemon.py > daemon.log 2> daemon.err &
+until python daemon.py; do
+    echo "The GP daemon aborted with exit code $?.  Respawning in 30 seconds..." >&2
+    sleep 30
+done
---
+++ fabfile.py
@@ -0,0 +1,43 @@
+from fabric.api import *
+
+env.roledefs = {
+    'master': ['ec2-user@sl-master.dyndns-server.com'],
+    'servant': ['ec2-user@ec2-184-73-138-89.compute-1.amazonaws.com',
+                'ec2-user@ec2-107-22-12-215.compute-1.amazonaws.com',
+                'ec2-user@ec2-184-72-65-151.compute-1.amazonaws.com',
+                'ec2-user@ec2-184-72-137-158.compute-1.amazonaws.com',
+				'ec2-user@ec2-50-17-1-67.compute-1.amazonaws.com',
+				'ec2-user@ec2-72-44-53-225.compute-1.amazonaws.com',
+				'ec2-user@ec2-107-22-142-165.compute-1.amazonaws.com',
+				'ec2-user@ec2-107-22-53-205.compute-1.amazonaws.com',
+				'ec2-user@ec2-23-20-67-140.compute-1.amazonaws.com'
+                ]
+}
+
+@roles('servant')
+@parallel
+def check_load():
+	run('ps -u ec2-user r -f | grep python | wc -l')
+
+@roles('servant')
+@parallel
+def push_update():
+	try:
+		run('killall python')
+	except:
+		pass
+	try:
+		run('rm -rf ~/.picloud/datalogs/Simulation')
+	except:
+		pass
+	with cd('~/devel/SocialLearning'):
+		run('git pull')
+
+@roles(['master', 'servant'])
+def change_passphrase():
+	run('ssh-keygen -p')
+
+@roles('servant')
+def start_daemon():
+	with cd('~/devel/SocialLearning'):
+		run('./daemon.sh')
