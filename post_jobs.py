664f5d4a0be6c53a21fa625d7d49b4246bd322e0
G-J van Rooyen <gvrooyen@sun.ac.za>
2012-02-26 02:00:17 +0200

    Fixed a few minor bugs in the post_jobs.py script

--- post_jobs.py
+++ post_jobs.py
@@ -60,12 +60,12 @@ print("Current progress:")
 pg = assess_progress()
 print pg
 
-for m in pg:
+for (idx,m) in enumerate(pg):
 	for i in xrange(0, MAX_GENERATIONS-pg[m][1]):
 		msg = Message()
-		msg.set_body(json.dumps(['-d ' + m + str(pg[m][1])] + modes[m]))
+		msg.set_body(json.dumps(['-d ' + m + str(pg[m][1])] + modes[idx][1]))
 		task_queue.write(msg)
-	for i in xrange(m[0]+1, MAX_DEMES):
+	for i in xrange(m[1][0]+1, MAX_DEMES):
 		msg = Message()
 		msg.set_body(json.dumps(['-d ' + m + str(i)] + modes[m]))
 		task_queue.write(msg)
